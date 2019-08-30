bdd857f5d04b2808f7e43921bb6fa3f7f154c9de
luxianming <luxianming@021.com>
2019-08-30 14:47:17 +0800

    图标引入完成

--- component/Home/Home.js
+++ component/Home/Home.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
+//图标
+import Feather from 'react-native-vector-icons/Feather';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
 export default class Home extends Component {
     render() {
         return (
             <View style={styles.Home}>
                 <Text>Home</Text>
+                <Feather name='feather' size={25} color='#10aeff'/>
             </View>
         )
     }
